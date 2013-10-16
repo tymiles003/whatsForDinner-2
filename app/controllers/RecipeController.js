2bb246c3e8ebb5defc6b2a710c32181eb76513ea
Justin Beal <justin.beal@mapquest.com>
2013-10-15 18:16:46 -0600

    Basic endpoints for RecipeController

--- app/controllers/RecipeController.js
+++ app/controllers/RecipeController.js
@@ -1,9 +1,27 @@
-module.exports = {
+var recipeExports = {
   getRecipe: function(req, res) {
-    res.send("Got recipe. Ingredient is ", ingredient);
+    var ingredients = req.query.ingredients; // comma delimited string
+
+    if(typeof ingredients === 'undefined') {
+      res.send("Undefined ingredients!");
+    } else {
+      var recipes = recipeExports._getRecipeFromDB(ingredients);
+      res.send("Got recipes -> " + recipes);
+    }
   },
 
   createRecipe: function(req, res) {
     res.send("Created recipe.");
+  },
+
+  _getRecipeFromDB: function(ingredients) {
+    var ingArr = ingredients.split(",")
+    console.log(ingArr);
+    
+    // need to find recipe nodes connected to ingredient nodes with a max SUBSTITUTES_FOR distance of 1 from supplied ingredients
+    return ingArr;
   }
 }
+
+module.exports = recipeExports;
+
